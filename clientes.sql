33766ec37e660308ce8511d95799d0bccec25b6d
juh <juliana.karling@escola.pr.gov.br>
2026-07-01 12:06:57 -0300

    Create clientes.sql

---
+++ clientes.sql
@@ -0,0 +1,9 @@
+INSERT INTO cliente (nome, telefone, cpf, endereco)
+VALUES
+('Maria Eduarda', '45f5545', '000.000.000-00', 'sei la nao entendi'),
+('Fernanda', '3435233', '000.000.000-01', 'sla nao entendii'),
+('Mario', '141324', '000.000.000-02', 'sla tambem'),
+('Joaquim', '1231231', '000.000.000-03', 'slasla'),
+('Italo Santos', '321312', '000.000.000-04', 'aaamnsla');
+
+SELECT * FROM cliente;
